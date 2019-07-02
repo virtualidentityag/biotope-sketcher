b9ac766ed2482bb5c147b3b2940d79d8b1bcd34e
Filipe Martins <filipe.martins@virtual-identity.com>
2019-07-02 12:06:32 +0100

    Update CLI

--- biotope-sketcher-cli/src/index.ts
+++ biotope-sketcher-cli/src/index.ts
@@ -1,28 +1,10 @@
-import {Command, flags} from '@oclif/command'
+import {Command} from '@oclif/command'
 
 class BiotopeSketcherCli extends Command {
-  static description = 'describe the command here'
-
-  static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
-
-  static args = [{name: 'file'}]
+  static description = 'It displays a log on the terminal'
 
   async run() {
-    const {args, flags} = this.parse(BiotopeSketcherCli)
-
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    this.log('thats me logging info for you')
   }
 }
 
