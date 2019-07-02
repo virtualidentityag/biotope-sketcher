975ba988451d4aa50311979f3d52f3a3eb2db366
Filipe Martins <41577958+filipemartinsfe@users.noreply.github.com>
2019-07-02 14:58:55 +0100

    Create cli (#1)
    * Create CLI
    
    * Update CLI

---
+++ biotope-sketcher-cli/src/index.ts
@@ -0,0 +1,11 @@
+import {Command} from '@oclif/command'
+
+class BiotopeSketcherCli extends Command {
+  static description = 'It displays a log on the terminal'
+
+  async run() {
+    this.log('thats me logging info for you')
+  }
+}
+
+export = BiotopeSketcherCli
